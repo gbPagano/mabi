2a01acf02b0eb8085f7c9780b209232a23715050
gbPagano <guilhermebpagano@gmail.com>
2024-11-24 13:54:59 -0300

    feat: changes the way the servo motor step is done

--- mabi-rp/src/main.rs
+++ mabi-rp/src/main.rs
@@ -1,4 +1,4 @@
-use gilrs::{Button, Event, EventType, Gilrs};
+use gilrs::{Axis, Button, Event, EventType, GamepadId, Gilrs};
 use mabi_rs::servo::Servo;
 use pwm_pca9685::{Address, Channel, Pca9685};
 use rppal::i2c::I2c;
@@ -10,69 +10,64 @@ fn main() {
         println!("{} id={id} is {:?}", gamepad.name(), gamepad.power_info());
     }
 
+    // initialize pwm controller
     let dev = I2c::new().unwrap();
     let address = Address::default();
-    let mut pwm = Pca9685::new(dev, address).unwrap();
+    let pwm = Pca9685::new(dev, address).unwrap();
 
     let mut arm = Arm {
         controller: pwm,
         base: Servo::new(Channel::C5, (0, 180), 90),
-        shoulder: Servo::new(Channel::C5, (0, 180), 90),
-        elbow: Servo::new(Channel::C5, (0, 180), 90),
-        wrist_vertical: Servo::new(Channel::C5, (0, 180), 90),
-        wrist_horizontal: Servo::new(Channel::C5, (0, 180), 90),
-        claw: Servo::new(Channel::C5, (0, 180), 90),
+        shoulder: Servo::new(Channel::C1, (0, 180), 90),
+        elbow: Servo::new(Channel::C1, (0, 180), 90),
+        wrist_vertical: Servo::new(Channel::C1, (0, 180), 90),
+        wrist_horizontal: Servo::new(Channel::C1, (0, 180), 90),
+        claw: Servo::new(Channel::C1, (0, 180), 90),
     };
 
     arm.init();
-
     loop {
-        arm.update_positions();
+        //arm.update_positions();
+        //if let Some((_, gamepad)) = gilrs.gamepads().next() {
+        //    //if let Some(data) = gamepad.button_data(Button::RightTrigger2) {
+        //    //println!("{:?}", data); }
+        //    //let code = gamepad.button_code(Button::RightTrigger2).unwrap();
+        //    let data = gamepad
+        //        .state()
+        //        .value(gamepad.button_code(Button::RightTrigger2).unwrap());
+        //    println!("{:?}", data);
+        //}
         while let Some(event) = gilrs.next_event() {
-            match event {
-                // Event { event: EventType::ButtonPressed(Button::South, _), .. } => {
-                //     println!("pressed: X")
-                // }
-                Event {
-                    event: EventType::AxisChanged(axis, val, _),
-                    ..
-                } if val.abs() > 0.2 => {
-                    println!("axis {:?} : {:?}", axis, val)
+            match event.event {
+                EventType::AxisChanged(axis, val, _) if val.abs() > 0.2 => {
+                    //let val = if val.abs() > 0.2 { val } else { 0.0 };
+                    match axis {
+                        Axis::LeftStickX => {
+                            println!("Left Stick X moved: {val}");
+                        }
+                        Axis::LeftStickY => {
+                            println!("Left Stick Y moved: {val}");
+                        }
+                        Axis::RightStickX => {
+                            println!("Right Stick X moved: {val}");
+                        }
+                        Axis::RightStickY => {
+                            println!("Right Stick Y moved: {val}");
+                        }
+                        _ => (),
+                    }
                 }
-                Event {
-                    event: EventType::ButtonChanged(Button::RightTrigger2, val, _),
-                    ..
-                } => {
-                    println!("pressing: R2 :: {val}");
-                    arm.base.step(val as f64);
-                }
-                Event {
-                    event: EventType::ButtonChanged(Button::LeftTrigger2, val, _),
-                    ..
-                } => {
-                    println!("pressing: L2 :: {val}");
-                    arm.base.step(-val as f64);
-                }
-                Event {
-                    event: EventType::ButtonPressed(btn, _),
-                    ..
-                } => {
-                    println!("pressed: {:?}", btn);
-                }
-                Event {
-                    id,
-                    event: EventType::Connected,
-                    ..
-                } => {
-                    println!("Gamepad connected :: id={id}")
-                }
-                Event {
-                    id,
-                    event: EventType::Disconnected,
-                    ..
-                } => {
-                    println!("Gamepad disconnected :: id={id}")
-                }
+                EventType::ButtonChanged(button, val, _) => match button {
+                    Button::RightTrigger2 => {
+                        println!("pressing: R2 :: {val}");
+                         arm.base.step_speed = val;
+                    }
+                    Button::LeftTrigger2 => {
+                        println!("pressing: L2 :: {val}");
+                         arm.base.step_speed = -val;
+                    }
+                    _ => (),
+                },
                 _ => (),
             };
         }
@@ -93,15 +88,40 @@ impl Arm {
     pub fn init(&mut self) {
         // This corresponds to a frequency of 60 Hz
         self.controller.set_prescale(121).unwrap();
-
         self.controller.enable().unwrap();
 
         self.update_positions();
     }
 
-    pub fn update_positions(&mut self) {
+    pub fn step(&mut self) {
+        self.base.step();
+        self.shoulder.step();
+        self.elbow.step();
+        //self.wrist_vertical.step();
+        //self.wrist_horizontal.step();
+        //self.claw.step();
+
+        self.update_positions();
+    }
+
+    fn update_positions(&mut self) {
         self.controller
             .set_channel_on_off(self.base.channel, 0, self.base.curr_duty)
             .unwrap();
+        self.controller
+            .set_channel_on_off(self.shoulder.channel, 0, self.shoulder.curr_duty)
+            .unwrap();
+        self.controller
+            .set_channel_on_off(self.elbow.channel, 0, self.elbow.curr_duty)
+            .unwrap();
+        //self.controller
+        //    .set_channel_on_off(self.wrist_vertical.channel, 0, self.wrist_vertical.curr_duty)
+        //    .unwrap();
+        //self.controller
+        //    .set_channel_on_off(self.wrist_horizontal.channel, 0, self.wrist_horizontal.curr_duty)
+        //    .unwrap();
+        //self.controller
+        //    .set_channel_on_off(self.claw.channel, 0, self.claw.curr_duty)
+        //    .unwrap();
     }
 }
--- mabi-rp/src/servo.rs
+++ mabi-rp/src/servo.rs
@@ -1,13 +1,12 @@
-use pwm_pca9685::{Address, Channel, Pca9685};
-use rppal::i2c::I2c;
-use std::thread;
-use std::time::Duration;
+use pwm_pca9685::Channel;
+
 
 pub struct Servo {
     pub channel: Channel,
     pub angle_range: (u8, u8),
     pub curr_duty: u16,
-    real_pos: f64, // in angle
+    pub speed: f32, // step speed
+    real_pos: f32, // in angle
 }
 
 impl Servo {
@@ -19,24 +18,25 @@ impl Servo {
             channel,
             angle_range,
             curr_duty: Self::angle_to_duty(start_angle),
-            real_pos: start_angle as f64,
+            real_pos: start_angle as f32,
+            speed: 0.,
         }
     }
 
     fn get_curr_angle(&self) -> u8 {
         (180.0
-            * ((self.curr_duty - Self::MIN_DUTY) as f64 / (Self::MAX_DUTY - Self::MIN_DUTY) as f64))
+            * ((self.curr_duty - Self::MIN_DUTY) as f32 / (Self::MAX_DUTY - Self::MIN_DUTY) as f32))
             .round() as u8
     }
 
     fn angle_to_duty(angle: u8) -> u16 {
-        ((angle as f64 / 180.0) * (Self::MAX_DUTY - Self::MIN_DUTY) as f64) as u16 + Self::MIN_DUTY
+        ((angle as f32 / 180.0) * (Self::MAX_DUTY - Self::MIN_DUTY) as f32) as u16 + Self::MIN_DUTY
     }
 
-    pub fn step(&mut self, val: f64) {
-        self.real_pos += val;
-        self.real_pos = self.real_pos.min(self.angle_range.1 as f64);
-        self.real_pos = self.real_pos.max(self.angle_range.0 as f64);
+    pub fn step(&mut self) {
+        self.real_pos += self.speed;
+        self.real_pos = self.real_pos.min(self.angle_range.1 as f32);
+        self.real_pos = self.real_pos.max(self.angle_range.0 as f32);
 
         self.curr_duty = Servo::angle_to_duty(self.real_pos as u8);
     }
@@ -62,7 +62,8 @@ mod tests {
             channel: Channel::C0,
             angle_range: (0, 0),
             curr_duty: Servo::MIN_DUTY,
-            real_pos: 0.0,
+            real_pos: 0.,
+            speed: 0.,
         };
         assert_eq!(servo.get_curr_angle(), 0);
 
